d304d26f14c2be37f2c6e3e45197beeb39905d86
abderrahmane <abderrahmaneosmani2@gmail.com>
2022-08-11 17:54:06 +0100

    create signin google firebase

--- firebase.js
+++ firebase.js
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "portfolio-9a3ea.firebaseapp.com",
   databaseURL: "https://portfolio-9a3ea-default-rtdb.firebaseio.com",
   projectId: "portfolio-9a3ea",
